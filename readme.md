3e7833d2b624c394a0cc854558b1f291a8662598
Leon Etienne <me@leonetienne.de>
2023-09-17 14:18:15 +0200

    feat: rename configuration.nix to master.nix

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 This is my systems nix configuration.  
-The master file is `configuration.nix` and includes all the other "sub configurations".
+The master file is `master.nix` and includes all the other "sub configurations".
--- zsh.nix
+++ zsh.nix
@@ -5,27 +5,28 @@
   programs.zsh = {
     enable = true;
     shellAliases = {
-    	ll = "ls -l";
-        nix-update = "sudo nixos-rebuild switch";
-        gda = "git add --all";
-        gst = "git status";
-        gcm = "git commit -m";
-        gush = "git push";
-        gull = "git pull";
-        glone = "git clone";
-        giff = "git diff";
-        gog = "git log";
-        open = "xdg-open";
+      ll = "ls -l";
+      nix-update = "sudo nixos-rebuild switch -I nixos-config=/etc/nixos/master.nix";
+      gda = "git add --all";
+      gst = "git status";
+      gcm = "git commit -m";
+      gush = "git push";
+      gull = "git pull";
+      glone = "git clone";
+      giff = "git diff";
+      gog = "git log";
+      open = "xdg-open";
     };
 
     histSize = 1000000;
     histFile = "$HOME/.zshhistory";
     ohMyZsh = {
-    	enable = true;
-	plugins = [
-	  "git"
-	];
-	theme = "robbyrussell";
+      enable = true;
+      plugins = [
+        "git"
+      ];
+      theme = "robbyrussell";
     };
   };
 }
+
