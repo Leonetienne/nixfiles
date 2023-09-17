a17ef0fa1bb6a69fbbc88faed07c235f7b768c29
Leon Etienne <me@leonetienne.de>
2023-09-17 14:03:52 +0200

    fix: ddev not working

--- ddev.nix
+++ ddev.nix
@@ -15,10 +15,17 @@
   ];
 
   # Allow Xdebug to use port 9003
-  networking.firewall.allowedTCPPorts = [ 9003 ];
+  networking.firewall.allowedTCPPorts = [
+    9003
+    443
+    80
+  ];
 
   # Make it possible for ddev to modify the /etc/hosts file.
   # Otherwise you'll have to manually change the
   # hosts configuration after creating a new ddev project.
   environment.etc.hosts.mode = "0644";
+
+  # Allow processes running as non-root to open all ports >= 80
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
 }
