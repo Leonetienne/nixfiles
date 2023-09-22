6b8f1ef9096937d8fcdfb13924a454461dd0e7fc
Leon Etienne <me@leonetienne.de>
2023-09-22 02:33:27 +0200

    configure: vi tabsize

--- users/agent/packages/nvim.nix
+++ users/agent/packages/nvim.nix
@@ -14,9 +14,9 @@
       syntax enable
       set number
       set expandtab
-      set tabstop=4
-      set shiftwidth=4
-      set softtabstop=4
+      set tabstop=2
+      set shiftwidth=2
+      set softtabstop=2
       map <C-n> :NERDTreeToggle<CR>
       let NERDTreeShowHidden = 1
     '';
